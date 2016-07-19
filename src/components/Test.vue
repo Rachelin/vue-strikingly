b3a5de8a8a292303a8a69a998d5a5764c1cf8e27
Rachelin <wuyou.spring@foxmail.com>
2016-07-19 18:10:34 +0800

    add a test vue

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="test">
+        <slot name="order"></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Test',
+    }
+</script>
+
+<style>
+    .test{
+        width: 80%;
+        height: 300px;
+        background-color: #fb2;
+        margin:50px auto;
+    }
+</style>
